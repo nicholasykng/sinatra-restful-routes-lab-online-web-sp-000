331d4d466c561191ef6c68b521c29db40ee550a6
Nicholas Ng <nicholasykng@gmail.com>
2020-01-05 22:16:56 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,12 @@ class ApplicationController < Sinatra::Base
     set :public_folder, 'public'
     set :views, 'app/views'
   end
-  
+
   get "/recipes" do
     @recipes = Recipe.all
     erb :index
   end
-  
+
   get "/recipes/new" do
     erb :new
   end
@@ -17,7 +17,7 @@ class ApplicationController < Sinatra::Base
     @recipe = Recipe.create(:name => params[:name], :ingredients => params[:ingredients], :cook_time => params[:cook_time])
     redirect to "/recipes/#{@recipe.id}"
   end
-  
+
   get "/recipes/:id" do
     @recipe = Recipe.find_by_id(params[:id])
     erb :show
